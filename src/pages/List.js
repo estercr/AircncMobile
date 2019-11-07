431b45930fb4ca8568aacb74d24453808a3bf4be
Ester Cristina <ester.cristina.silva@accenture.com>
2019-11-07 13:42:14 -0300

    adding lists and spots in home

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, AsyncStorage } from 'react-native';
+import {
+    SafeAreaView,
+    Text,
+    Image,
+    ScrollView,
+    StyleSheet,
+    AsyncStorage
+} from 'react-native';
+
+import SpotList from '../components/SpotList';
+import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
@@ -11,5 +21,26 @@ export default function List() {
             setTechs(techsArray);
         })
     }, [])
-    return <View>{techs}</View>
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+})
