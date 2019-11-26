7d58228e5cab01ad9a631bca084a8cca75ff243a
Ester Cristina <ester.cristina.silva@accenture.com>
2019-11-26 10:04:13 -0300

    finalization of the mobile part

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
 import {
     SafeAreaView,
-    Text,
+    Alert,
     Image,
     ScrollView,
     StyleSheet,
@@ -13,6 +14,19 @@ import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.43.46:3333', {
+                query : { user_id }
+            })
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+                console.log('teste')
+            })
+            console.log(socket)
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storageTechs => {
